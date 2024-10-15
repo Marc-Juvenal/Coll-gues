413fbd9040a4d3b2fa515caf1a3f1fde3f0b0fd1
Marc Juvénal <marcjuvenal95@gmail.com>
2024-10-15 12:10:48 +0200

    Ajout du fichier collègues

---
+++ collègues.mjs
@@ -0,0 +1,15 @@
+import chalk from 'chalk';
+
+const colleagues = [
+    'Camille',
+    'Joey',
+    'Manon',
+    'Antoine',
+];
+
+console.log(chalk.red(colleagues[0]));
+console.log(chalk.green(colleagues[1]));
+console.log(chalk.blue(colleagues[2]));
+console.log(chalk.yellow(colleagues[3]));
+
+
